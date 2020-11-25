a1543a38d388033dff3a0f4280080642f9d40d99
Arpit Jain <abs.jain16@gmail.com>
2020-11-25 16:38:55 +0530

    Add in clustering

---
+++ cluster.js
@@ -0,0 +1,25 @@
+global.CONSTANTS = require(__dirname + "/lib/constants.js");
+
+const cluster = require("cluster");
+
+if (cluster.isMaster) {
+    const conf = require(CONSTANTS.CONFDIR + "/cluster.json");
+
+    // Figure out number of workers.
+    let numWorkers = conf.workers;
+    if (numWorkers == 0) {
+        const numCPUs = require("os").cpus().length;
+        if (numCPUs < conf.min_workers) numWorkers = conf.min_workers;
+        else numWorkers = numCPUs;
+    }
+
+    // Fork workers.
+    console.log("Starting " + numWorkers + " workers.");
+    for (let i = 0; i < numWorkers; i++) cluster.fork();
+
+    cluster.on("exit", (server, _code, _signal) => {
+        console.log("Worker server with PID: " + server.process.pid + " died.");
+        console.log("Forking a new process to compensate.");
+        cluster.fork();
+    });
+} else require(CONSTANTS.ROOTDIR + "/echo.js").bootstrap();
