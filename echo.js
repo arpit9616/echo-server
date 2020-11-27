c7ab2ecc110deb6fd90bdd297fe9352a97a0f6a3
Arpit Jain <abs.jain16@gmail.com>
2020-11-27 13:09:15 +0530

    Better logs and conf check

--- echo.js
+++ echo.js
@@ -5,9 +5,11 @@ const bootstrap = () => {
     const conf = require(`${CONSTANTS.CONFDIR}/server.json`);
 
     // Init TCP Echo Server
+    console.log("Starting TCP Server...");
     if (conf.tcp) require(`${CONSTANTS.LIBDIR}/tcp_server.js`).initTCPServer();
     
     // Init UDP Echo Server
+    console.log("Starting UDP Server...");
     if (conf.udp) require(`${CONSTANTS.LIBDIR}/udp_server.js`).initUDPServer();
 
 };
